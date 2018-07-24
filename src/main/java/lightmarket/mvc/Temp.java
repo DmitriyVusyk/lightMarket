183436095732bf33b97702d266649874e593371a
DmitriyVusyk <kotmurkot4502@gmail.com>
2018-07-24 19:26:03 +0200

    s

--- src/main/java/lightmarket/mvc/Temp.java
+++ src/main/java/lightmarket/mvc/Temp.java
@@ -1,4 +1,11 @@
 package lightmarket.mvc;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class Temp {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String password = encoder.encode("admin");
+        System.out.println(password);
+    }
 }
