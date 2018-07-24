17f3c546981fd990048e2884a3fb7eb693e6148a
DmitriyVusyk <kotmurkot4502@gmail.com>
2018-07-24 18:30:21 +0200

    s

--- src/main/java/lightmarket/mvc/controller/config/SecurityConfig.java
+++ src/main/java/lightmarket/mvc/controller/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
+    @Lazy
     private DataSource dataSource;
 
     @Value("${spring.queries.users-query}")
